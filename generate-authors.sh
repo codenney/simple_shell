2735ff9ad24b001823477afd2fb589a47b1d4d59
Olaitan Adedokun <kenney9224@gmail.com>
2023-04-20 10:37:32 +0100

    edited the script and add the generated AUTHORS file

--- generate-authors.sh
+++ generate-authors.sh
@@ -10,8 +10,6 @@ set -x
 cat > "${ROOTDIR}/AUTHORS" <<- EOF
 	# This is the pre-fill content before the contents. DO NOT EDIT, PLEASE!
 	# This file create a list of all contributors to the repository.
-	# Alt command to use on your terminal instead is:
-    # git shortlog -sne --no-merges | awk '{print $2, $3}' > AUTHORS |
-    # git shortlog -sne --no-merges | awk '{print $2, "<"$3">"}' > AUTHORS
+	#
 	$(git -C "$ROOTDIR" log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf)
 EOF
