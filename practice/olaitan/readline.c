6ec1d03e9dbd079689c5adce4e76e78e86186eb2
Olaitan Adedokun <kenney9224@gmail.com>
2023-04-21 13:39:14 +0100

    A program that print and wait for the user to enter an input

---
+++ practice/olaitan/readline.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - function entry
+ * Description: A program that prints "$ ",
+ * wait for the user to enter a command,
+ * prints it on the next line.
+ * Return: 0 on success
+ */
+int main(void)
+{
+	size_t n = 10;
+	size_t bufsize;
+	char *buf = NULL;
+
+	printf("$ ");
+	bufsize = getline(&buf, &n, stdin);
+	if (bufsize == -1)
+		perror("Error\n");
+
+	printf("%s", buf);
+	free(buf);
+}
