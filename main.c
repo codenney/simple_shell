a5aad0156a50a2c33b8b0ec0168d27dc608a4590
Olaitan Adedokun <kenney9224@gmail.com>
2023-04-25 03:57:24 +0100

    starter files

---
+++ main.c
@@ -0,0 +1,11 @@
+/**
+ * main - Runs a simple UNIX command interpreter.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: The return value of the last executed command.
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
