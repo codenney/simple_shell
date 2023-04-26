481fea8beeb8a6ede7e6391e5f40938fcae4e451
Olaitan Adedokun <kenney9224@gmail.com>
2023-04-26 07:47:36 +0100

    start

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * main - main entry for the program
  * @argc: The number of arguments supplied to the program
@@ -6,5 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
+	int returnLine;
+	char *cmd = NULL;
+	size_t n = 0;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		returnLine = getline(&cmd, &n, stdin);
+		if (returnLine == -1)
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			free(cmd);
+		}
+	}
+	free(cmd);
 	return (0);
 }
