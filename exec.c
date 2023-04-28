8eb22b51591fb0aec6e63e5578e63d35e5163c12
Olaitan Adedokun <kenney9224@gmail.com>
2023-04-28 07:29:16 +0100

    betty fixed

--- exec.c
+++ exec.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
+/**
+ * execmd - function entry
+ * Description: ----
+ * @argv: argument vector
+ */
 void execmd(char **argv)
 {
-    pid_t pid;
-    int status;
-    char *actual_command = NULL;
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("Error: ");
-        /*return (-1);*/
-    }
-    else if ((argv) && (pid == 0))
-    {
-        actual_command = argv[0];
-        if (execve(actual_command, argv, environ) == -1)
-        {
-            perror("Error: ");
-        }
-        exit(0);
-    }
-    else
-    {
-        wait(&status);
-    }
-}
+	pid_t pid;
+	int status;
+	char *actual_command = NULL;
 
+	pid = fork();
+
+	if (pid == -1)
+		perror("Error: ");
+	else if ((argv) && (pid == 0))
+	{
+		actual_command = argv[0];
+		if (execve(actual_command, argv, environ) == -1)
+		{
+			perror("Error: ");
+		}
+		exit(0);
+	}
+	else
+		wait(&status);
+}
